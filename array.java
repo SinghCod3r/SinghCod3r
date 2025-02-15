8659548f725e815907f8f6b5eda1927e999c514e
Ayush Singh <ayushsinghceee@gmail.com>
2025-02-15 19:48:26 +0530

    Third Largest and Third Smallest Element
    QnA

--- array.java
+++ array.java
@@ -1,10 +1,14 @@
 import java.util.*;
+
 public class array{
     public static void main(String args[]){
         int n = 5;
         int[] arr = {1,8,3,1,5};
+
         Arrays.sort(arr);
-        System.out.println(arr[arr.length-2]);
-        System.out.println(arr[1]);
+        
+
+        System.out.println(arr[arr.length-3]);
+        System.out.println(arr[2]);
     }
 }
